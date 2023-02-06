07cd90aafb7471c43062acbb1026a2e29951f340
potatoJs <103060805+potato-Js@users.noreply.github.com>
2023-02-06 20:19:20 +0000

    commit made

--- dual_display.sh
+++ dual_display.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+inFo=$( xrandr --listactivemonitors )
+disPlay=$( xrandr --listactivemonitors | grep "Monitors:" | awk '{ print $2 }');
 
+if [ $disPlay -eq 1 ];then
+    printf "ERR: %s\n%s\n" "only one disPlay found" "$inFo"
+    exit 1;
+fi
 
-disPlay=$( xrandr --listactivemonitors | grep "Monitor:" | awk '{ print $2 }');
-printf "%s\n" "$disPlay"
-
-#xrandr --output eDP1 --primary --mode 1920x1080 --rotate normal --output HDMI1 --mode 1920x1080 --rotate normal --right-of eDP1
+if [ $disPlay -eq 2 ];then
+    printf "%s\n" "eDP1 is always primary and on left, HDMI1 is on right and always secondary"
+    xrandr --output eDP1 --primary --mode 1920x1080 --rotate normal --output HDMI1 --mode 1920x1080 --rotate normal --right-of eDP1
+fi
