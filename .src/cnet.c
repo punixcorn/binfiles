af5233c3ad953a55121465ba4eb98143d3161f7b
potatoJs <103060805+potato-Js@users.noreply.github.com>
2023-03-12 17:44:06 +0000

    added  a make file

--- .src/cnet.c
+++ .src/cnet.c
@@ -1,35 +1,33 @@
-#include "binHeaders.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 char command[100];
+void Error(char *name) {
+  fprintf(stderr,
+          "ERR : Invaild values passed\n"
+          "Usage : %s [ number of pings ]\n",
+          name);
+  exit(1);
+}
 int main(int argc, char **argv) {
   sprintf(command, "ping archlinux.org");
-  if (argc == 2) {
-    if (*(*(argv + 1)) == '-' && *(*(argv + 1) + 1) == 'h') {
-      fprintf(stdout, "Useage : %s [ number of pings ]\n", *(argv));
-      exit(0);
-    }
-    int check = 0;
-    if ((check = atoi(*(argv + 1))) == 0) {
-      fprintf(stderr,
-              "ERR : Invaild values passed\n"
-              "Useage : %s [ number of pings ]\n",
-              *(argv));
-      exit(1);
-    }
-    goto arg2;
-  } else if (argc > 2) {
-    fprintf(stderr,
-            "ERR : Invaild values passed\n"
-            "Useage : %s [ number of pings ]\n",
-            *(argv));
-    exit(1);
+#if DEBUG
+  printf("this argc = %d\n", argc);
+#endif
+  if (argc == 1) {
+    system(command);
+    exit(0);
   }
-  system(command);
-  exit(0);
-arg2:
+  if (argc > 2)
+    Error(*(argv));
+  if (*(*(argv + 1)) == '-' && *(*(argv + 1) + 1) == 'h') {
+    fprintf(stdout, "Usage : %s [ number of pings ]\n", *(argv));
+    exit(0);
+  }
+  if (atoi(*(argv + 1)) == 0) {
+    Error(*(argv));
+  }
   strcat(command, " -c");
   strcat(command, *(argv + 1));
   system(command);
