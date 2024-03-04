f0ddbcfc216ecd9a71baa0ea9a9665c5ed199908
punixcorn <ohyunwoods663@gmail.com>
2024-03-04 00:04:52 +0000

    minor changes made

--- src/underDevelopment/_msd.cxx
+++ src/underDevelopment/_msd.cxx
@@ -14,88 +14,121 @@ using namespace fmt;
 // -p place <disk>
 // -d <disk> <place>
 
-class Parser : public Engine {
-   private:
+class Parser : public Engine
+{
+  private:
     int Argc;
     char **Argv;
 
-   public:
-    Parser(int argc, char **argv) : Argc(argc), Argv(argv) {
-        if (argc <= 1) {
+  public:
+    Parser(int argc, char **argv) : Argc(argc), Argv(argv)
+    {
+        if (argc <= 1)
+        {
             fmt::print("Error Invalid arguments passed\n");
             exit(1);
         }
         parse();
     };
 
-    ~Parser() { delete this; }
+    ~Parser()
+    {
+        delete this;
+    }
 
-    void parse() {
+    void parse()
+    {
         bool disk{false}, place{false};
         std::string disk_p{}, place_p{};
         int nonParamCount{0};
-        switch (Argc) {
-            case 2:
-                if (isOptionFound(string(Argv[1]), "--help")) {
-                    print("{}", "-h,--help");
-                    exit(0);
-                } else {
-                    ERR("Invalid arguments passed", 1);
-                }
-                break;
-            default:
-                int i{1};
+        switch (Argc)
+        {
+        case 2:
+            if (isOptionFound(string(Argv[1]), "--help"))
+            {
+                print("{}", "-h,--help");
+                exit(0);
+            }
+            else
+            {
+                ERR("Invalid arguments passed", 1);
+            }
+            break;
+        default:
+            int i{1};
 
-                while (i < Argc) {
-                    if (isOptionFound(Argv[i], "--place")) {
-                        ++i;
-                        check_next(Argv[i],
-                                   "--place needs an argument, where to mount "
-                                   "the disk");
-                        place = true;
-                        try {
-                            place_p =
-                                std::string(Argv[i] == NULL ? throw : Argv[i]);
-                        } catch (const std::logic_error &e) {
-                            fmt::print("{}", e.what());
-                        }
-
-                    } else if (isOptionFound(Argv[i], "--disk")) {
-                        ++i;
-                        check_next(
-                            Argv[i],
-                            "--disk needs an argument, which disk to mount");
-                        disk = true;
-                        disk_p = std::string(Argv[i]);
-                    } else {
-                        nonParamCount++;
+            while (i < Argc)
+            {
+                if (isOptionFound(Argv[i], "--place"))
+                {
+                    ++i;
+                    check_next(Argv[i], "--place needs an argument, where to mount "
+                                        "the disk");
+                    place = true;
+                    try
+                    {
+                        place_p = std::string(Argv[i] == NULL ? throw : Argv[i]);
                     }
-                    i++;
-                };  // while
+                    catch (const std::logic_error &e)
+                    {
+                        fmt::print("{}", e.what());
+                    }
+                }
+                else if (isOptionFound(Argv[i], "--disk"))
+                {
+                    ++i;
+                    check_next(Argv[i], "--disk needs an argument, which disk to mount");
+                    disk = true;
+                    disk_p = std::string(Argv[i]);
+                }
+                else
+                {
+                    nonParamCount++;
+                }
+                i++;
+            }; // while
 
-                break;
+            break;
         };
 
-        if (nonParamCount == 0) {
-            if (disk && place) {
+        if (nonParamCount == 0)
+        {
+            if (disk && place)
+            {
                 print("{}\n", "disk && place passed");
-            } else {
+            }
+            else
+            {
                 ERR("Invalid args! at disk && place nonParamCount == 0");
             }
-        } else if (nonParamCount == 1) {
-            if (disk) {
+        }
+        else if (nonParamCount == 1)
+        {
+            if (disk)
+            {
                 print("{}\n", "disk passed, one arg");
-            } else if (place) {
+            }
+            else if (place)
+            {
                 print("{}\n", "place passed, one arg");
-            } else {
+            }
+            else
+            {
                 ERR("Invalid args! at disk || place nonParamCount == 1");
             }
-        } else if (nonParamCount == 2) {
+        }
+        else if (nonParamCount == 2)
+        {
             print("{}\n", "2 params");
-        } else {
+        }
+        else
+        {
             ERR("ALL FAILED");
         }
     }
 };
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv)
+{
+    return 0;
+}
--- src/underDevelopment/engine.hpp
+++ src/underDevelopment/engine.hpp
@@ -1,22 +1,23 @@
+#include <algorithm>
 #include <fmt/core.h>
 #include <fmt/format.h>
-
-#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 
 using fmt::print, fmt::format, std::string, std::vector;
 
-// -h,--help | this shows the help message
-
-struct Engine {
-   public:
+// this class is inherited and void parse(){} is developed in other files
+struct Engine
+{
+  public:
     int Argc;
     char **Argv;
 
-    Engine(int argc, char **argv) : Argc(argc), Argv(argv) {
-        if (argc <= 1) {
+    Engine(int argc, char **argv) : Argc(argc), Argv(argv)
+    {
+        if (argc <= 1)
+        {
             fmt::print("Error Invalid arguments passed\n");
             exit(1);
         }
@@ -27,40 +28,50 @@ struct Engine {
     Engine(Engine &&e) = default;
     Engine(Engine *e);
 
-    ~Engine() { delete this; }
+    ~Engine()
+    {
+        delete this;
+    }
 
     //
-    void ERR(string str, int status = 1) {
+    void ERR(string str, int status = 1)
+    {
         fmt::print(stderr, "{} : [ERR] {}\n", program_invocation_name, str);
         exit(status);
     }
 
     //
-    [[nodiscard("returns true if the str starts with a -")]] bool Is_param(
-        const char *str) {
+    [[nodiscard("returns true if the str starts with a -")]] bool Is_param(const char *str)
+    {
         return false;
     }
 
     //
-    bool isOptionFound(std::string haystack, std::string needle,
-                       bool checkShort = true) {
-        if (haystack == needle) return true;
+    bool isOptionFound(std::string haystack, std::string needle, bool checkShort = true)
+    {
+        if (haystack == needle)
+            return true;
         if (checkShort)
-            if (haystack == needle.substr(1, 2)) return true;
+            if (haystack == needle.substr(1, 2))
+                return true;
         return false;
     };
 
     // change string into lowerCase
-    void lowerCase(std::string &data) {
-        if (data.size() == 0) return;
-        std::transform(data.begin(), data.end(), data.begin(),
-                       [](unsigned char c) { return std::tolower(c); });
+    void lowerCase(std::string &data)
+    {
+        if (data.size() == 0)
+            return;
+        std::transform(data.begin(), data.end(), data.begin(), [](unsigned char c) { return std::tolower(c); });
     }
 
     // checks if the str starts with a '-'
-    void check_next(std::string str, std::string err_message) {
-        if (str.size() != 0) {
-            if (str[0] != '-') {
+    void check_next(std::string str, std::string err_message)
+    {
+        if (str.size() != 0)
+        {
+            if (str[0] != '-')
+            {
                 return;
             }
         }
@@ -69,4 +80,4 @@ struct Engine {
 
     void parse(){};
 
-};  // void Parse
+}; // void Parse
